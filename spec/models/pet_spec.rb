7123e6e038d7c712648f7589bdc8a7a38a6d146c
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-26 20:10:28 -0600

    Style: change before each block to let blocks and correct instance and class methods in tests

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -19,13 +19,13 @@ RSpec.describe Pet, type: :model do
   let!(:pet_3) { shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false) }
 
   describe 'class methods' do
-    describe '.search' do
+    describe '::search' do
       it 'returns partial matches' do
         expect(Pet.search("Claw")).to eq([pet_2])
       end
     end
 
-    describe '.adoptable' do
+    describe '::adoptable' do
       it 'returns adoptable pets' do
         expect(Pet.adoptable).to eq([pet_1, pet_2])
       end
